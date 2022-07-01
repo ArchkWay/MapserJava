17d6c76c715b92db031e7e1c5426a0caef27c652
Viktor Ovchinnikov <v.ovchinnikov@1er.app>
2022-07-01 13:06:41 +0300

    developed continuation lines to screen bounds

--- app/src/main/java/com/example/mapserjava/MapsActivity.java
+++ app/src/main/java/com/example/mapserjava/MapsActivity.java
@@ -16,16 +16,23 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnCameraMoveListener {
 
     GoogleMap mMap = null;
     ActivityMapsBinding binding = null;
-    LatLng locationA = null;
+    LatLng locationA = new LatLng(0.0, 0.0);
+    LatLng locationB = new LatLng(0.0, 0.0);
     int countMarkers = 0;
     double destination = 0.002;
+    LatLng southWest = null;
+    LatLng northEast = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -55,11 +62,89 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             createMarker(mMap, getString(R.string.b), view);
         });
     }
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
         LatLng rostov = new LatLng(47.2313, 39.7233); //default location Rostov
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(rostov));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rostov, 18f));
+        mMap.setOnCameraMoveListener(this);
+        setScreenBounds();
+    }
+
+
+    private void setLocations(LatLng locationB) {
+                createLine(locationA, locationB);
+                createLine(
+                        new LatLng(locationA.latitude - destination, locationA.longitude - destination),
+                        new LatLng(locationB.latitude - destination, locationB.longitude - destination));
+                createLine(
+                        new LatLng(locationA.latitude + destination, locationA.longitude + destination),
+                        new LatLng(locationB.latitude + destination, locationB.longitude + destination));
+
+    }
+
+    private void createLine(LatLng a, LatLng b) {
+        LatLng pointA = new LatLng(a.latitude, a.longitude);
+        LatLng pointB = new LatLng(b.latitude, b.longitude);
+
+        PolylineOptions polylineOptions = new PolylineOptions();
+        List<LatLng> pointsList = generateNeedingPoints(pointA, pointB);
+        for(int i = 0; i < pointsList.size(); i++) {
+            polylineOptions.add(pointsList.get(i));
+        }
+        mMap.addPolyline(polylineOptions);
+    }
+
+    private void polyline(LatLng locationB) {
+        createLine(locationA, locationB);
+        createLine(new LatLng(locationA.latitude - destination, locationA.longitude - destination),
+                new LatLng(locationB.latitude - destination, locationB.longitude - destination));
+        createLine(new LatLng(locationA.latitude + destination, locationA.longitude + destination),
+                new LatLng(locationB.latitude + destination, locationB.longitude + destination));
+    }
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+
+    private void setScreenBounds() {
+        LatLngBounds bounds = mMap.getProjection().getVisibleRegion().latLngBounds;
+        southWest = bounds.southwest;
+        northEast = bounds.northeast;
+    }
+
+    private List<LatLng> generateNeedingPoints(LatLng a, LatLng b )  {
+        setScreenBounds();
+        LatLng c;
+        LatLng d;
+        double longitudeC;
+        double longitudeD;
+        if (a.longitude < b.longitude) longitudeC = northEast.longitude;
+        else longitudeC = southWest.longitude;
+        if (a.longitude < b.longitude) {
+            longitudeD = southWest.longitude;
+        } else longitudeD = northEast.longitude;
+        c = calculateEdgeButton(a, b, longitudeC);
+        d = calculateEdgeButton(b, a, longitudeD);
+        List<LatLng> list = new ArrayList();
+        list.add(d);
+        list.add(a);
+        list.add(b);
+        list.add(c);
+        return list;
+    }
+
+    private LatLng calculateEdgeButton(LatLng a, LatLng b, Double edgeLng) {
+        double longChange = b.longitude - a.longitude;
+        double latChange = b.latitude - a.latitude;
+        double slope = latChange / longChange;
+        double longChangePointC = edgeLng - b.longitude;
+        double latChangePointC = longChangePointC * slope;
+        double latitudeC = b.latitude + latChangePointC;
+        return new LatLng(latitudeC, edgeLng);
     }
 
     private void createMarker(GoogleMap mMap, String title, View button){
@@ -67,8 +152,15 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             countMarkers++;
             mMap.setOnMapClickListener(latLng -> {
                 switch (countMarkers) {
-                    case (1): locationA = latLng;
-                    case (2): polyline(latLng);
+                    case (1): {
+                        locationA = latLng;
+                        break;
+                    }
+                    case (2): {
+                        polyline(latLng);
+                        locationB = latLng;
+                        break;
+                    }
                 }
                 mMap.addMarker(new MarkerOptions().position(latLng).title(title));
                 mMap.setOnMapClickListener(l  -> {});
@@ -77,23 +169,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    private void polyline(LatLng locationB) {
-        createPolyline(locationA, locationB);
-        createPolyline(new LatLng(locationA.latitude - destination, locationA.longitude - destination),
-                new LatLng(locationB.latitude - destination, locationB.longitude - destination));
-        createPolyline(new LatLng(locationA.latitude + destination, locationA.longitude + destination),
-                new LatLng(locationB.latitude + destination, locationB.longitude + destination));
-    }
-
-    private void createPolyline(LatLng a, LatLng b) {
-        PolylineOptions polylineOptions = new PolylineOptions()
-                .add(new LatLng(a.latitude, a.longitude))
-                .add(new LatLng(b.latitude, b.longitude));
-        mMap.addPolyline(polylineOptions);
-    }
-
     @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-        super.onPointerCaptureChanged(hasCapture);
+    public void onCameraMove() {
+        setScreenBounds();
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(locationA));
+        mMap.addMarker(new MarkerOptions().position(locationB));
+        setLocations(locationB);
     }
 }
